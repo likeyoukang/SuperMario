415ca252432be9da95eb179352d03cded98ebc5b
likeyoukang <49143902+likeyoukang@users.noreply.github.com>
2019-05-08 22:29:35 +0800

    Add files via upload

---
+++ trailblazers-base/src/main/java/edu/zut/hb/software/base/dao/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Data Access Object (dao) Base Classes
+ * 
+ * @author liuxiaoming
+ *
+ */
+package edu.zut.hb.software.base.dao;
---
+++ trailblazers-base/src/main/java/edu/zut/hb/software/base/domain/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 领域实体基类
+ *
+ * @author liuxiaoming
+ */
+/**
+ * @author liuxiaoming
+ *
+ */
+package edu.zut.hb.software.base.domain;
